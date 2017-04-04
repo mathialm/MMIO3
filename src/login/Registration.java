60e361ecf13c375e6d648861f1082cc3ac239212
KimRobin <KimRobin@10.22.47.27>
2017-04-04 13:21:29 +0200

    Removed unnecessary code

--- src/login/Registration.java
+++ src/login/Registration.java
@@ -4,12 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Registration {
 
-	HashMap user = new HashMap();
 	Scanner scanner = new Scanner(System.in);
 
 	public void registerUser() {
